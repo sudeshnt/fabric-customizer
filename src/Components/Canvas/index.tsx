8f4384efd04f10ee6865d4d58db333511e7bfb77
Sudesh Nimesha <sudeshnt93@gmail.com>
2022-06-13 14:08:10 +0700

    fix

--- src/Components/Canvas/index.tsx
+++ src/Components/Canvas/index.tsx
@@ -1,11 +1,8 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { fabric } from "fabric";
 import { useCanvasRef } from "../../Hooks/useCanvas";
-import { useDispatch } from "react-redux";
-import { Dispatch } from "../../Store";
 
 const Canvas = () => {
-  const dispatch = useDispatch<Dispatch>();
   const fabricCanvasRef = useCanvasRef();
   const canvasParentElementRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -25,9 +22,7 @@ const Canvas = () => {
   }, []);
 
   return (
-    // <div className="canvas-container" ref={canvasParentElementRef}>
     <canvas id="canvas" ref={canvasRef} width="300" height="1000"></canvas>
-    // </div>
   );
 };
 
